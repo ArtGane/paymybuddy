ac324084fb66da0c3bc633eceda743e1177394c0
ArtGane <argane.samson@gmail.com>
2023-02-07 17:32:18 +0100

    modification du if pour le create user

--- src/main/java/com/paymybuddy/webapp/service/UserService.java
+++ src/main/java/com/paymybuddy/webapp/service/UserService.java
@@ -3,12 +3,14 @@ package com.paymybuddy.webapp.service;
 import com.paymybuddy.webapp.entity.User;
 import com.paymybuddy.webapp.repository.UserRepository;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -25,6 +27,10 @@ public class UserService {
             user.setPassword(cryptPass);
 
             userRepository.save(user);
+            log.info("User " + user.getPseudo() + " est bien enregistré dans la base de données");
+
+        } else {
+            log.error("User non enregistré dans la base de données");
         }
     }
 }
