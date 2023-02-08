f66ada5685bafecea3d705b567bc6363cc1ef0ca
ArtGane <argane.samson@gmail.com>
2023-02-08 18:05:34 +0100

    ajoute de la méthode de login fonctionnelle avec le front login.html

---
+++ src/main/java/com/paymybuddy/webapp/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.paymybuddy.webapp.controller;
+
+import com.paymybuddy.webapp.model.UserLogin;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public ModelAndView showLoginForm() {
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("userLogin", new UserLogin());
+
+        return new ModelAndView("login", model);
+    }
+}
---
+++ src/main/java/com/paymybuddy/webapp/model/UserLogin.java
@@ -0,0 +1,14 @@
+package com.paymybuddy.webapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLogin {
+
+    private String email;
+    private String password;
+}
