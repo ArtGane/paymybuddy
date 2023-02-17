8c1abfd587fd43c708a87a47e44ef3a4a6861fec
ArtGane <argane.samson@gmail.com>
2023-02-17 20:06:03 +0100

    method get to post + ajout de CommandLineRunner pour implémenter des beans supplémentaires requis

--- src/main/java/com/paymybuddy/webapp/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/webapp/PayMyBuddyApplication.java
@@ -1,13 +1,22 @@
 package com.paymybuddy.webapp;
 
+import com.paymybuddy.webapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayMyBuddyApplication {
+public class PayMyBuddyApplication implements CommandLineRunner {
+
+    @Autowired
+    UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(PayMyBuddyApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
